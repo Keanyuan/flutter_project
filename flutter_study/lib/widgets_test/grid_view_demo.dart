e6aa70d9d137b0f45e9e295127a7743597055e6f
qizhiyuan <617293977@qq.com>
2019-01-02 21:49:34 +0800

    提交

--- flutter_study/lib/widgets_test/grid_view_demo.dart
+++ flutter_study/lib/widgets_test/grid_view_demo.dart
@@ -45,35 +45,40 @@ class _GridViewWidgetState extends State<GridViewWidgetDemo> {
 //ListView
 class _GridViewGridDelegateState extends State<GridViewWidgetDemo> {
 
+  var imageUrls = [
+    "http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg",
+    'http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',
+    'http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg'
+  ];
   @override
   void initState() {
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
+    imageUrls = imageUrls.reversed.toList();
 
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("GridView"),
       ),
-      body: GridView(
+      body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 2.0,
-            crossAxisSpacing: 2.0,
-            childAspectRatio: 0.7, //宽高比
+          crossAxisCount: 3,
+          mainAxisSpacing: 2.0,
+          crossAxisSpacing: 2.0,
+          childAspectRatio: 0.7, //宽高比
         ),
-        children: <Widget>[
-          new Image.network('http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',fit: BoxFit.cover),
-          new Image.network('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',fit: BoxFit.cover),
-        ],
+        itemBuilder: (BuildContext context, int index){
+          return new Image.network(imageUrls[index],fit: BoxFit.cover);
+        },
+        itemCount: imageUrls.length,
       )
     );
   }
