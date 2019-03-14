345f3bb6bc1eeea8ba8ca800140b45141972a6c6
qizhiyuan <qizhiyuan@anji-plus.com>
2019-03-14 14:08:19 +0800

    feat

--- kaiyan_client/lib/page/subscription/subscription.dart
+++ kaiyan_client/lib/page/subscription/subscription.dart
@@ -26,49 +26,92 @@ class _SubscriptionController extends State<SubscriptionController> with SingleT
   @override
   Widget build(BuildContext context) {
     return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
-        return [
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[Works(), Dynamic()],
+      ),
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
           SliverAppBar(
             floating: true,
             pinned: true,
             title: Text('subscription',
-                style: TextStyle(color: Colors.white, fontFamily: 'Lobster')),
+                style: TextStyle(color: Colors.black, fontFamily: 'Lobster')),
             centerTitle: true,
-            backgroundColor: const Color(0xffff26c9),
+            backgroundColor: Colors.white,
             bottom: PreferredSize(
-                child: Container(
-                  height: 150.0,
-                  child: new MyTabBar(
-                    labelColor: Colors.red,
-                    unselectedLabelColor: Colors.grey[100],
-                    indicator: KaiyanIndictor(w: 150.0, color: Colors.red),
-                    tabs: <MyTab>[
-                      new MyTab(
-                        child: Container(
-                          child: Center(child: Text("作品", style: TextStyle(fontSize: 40.0),)),
-                          decoration: new BoxDecoration(image: new DecorationImage(image: new ExactAssetImage(GSYICons.HOME_TOP_BG), fit: BoxFit.cover)),
-                        )),
-                      new MyTab(
-                          child: Container(
-                            child: Center(child: Text("动态", style: TextStyle(fontSize: 40.0))),
-                            decoration: new BoxDecoration(image: new DecorationImage(image: new ExactAssetImage(GSYICons.HOME_TOP_BG), fit: BoxFit.cover)),
-                          )),
-                    ],
-                    controller: _tabController,
-                  ),
+              preferredSize: Size(double.infinity, 34.0),
+              child: Container(
+                height: 34.0,
+                child: new TabBar(
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey[400],
+                  indicator: KaiyanIndictor(),
+                  tabs: <Tab>[
+                    new Tab(
+                      text: "作品",
+                    ),
+                    new Tab(
+                      text: "动态",
+                    ),
+                  ],
+                  controller: _tabController,
                 ),
-                preferredSize: Size(double.infinity,175.0)
+              ),
             ),
           )
         ];
       },
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          Works(),
-          Dynamic(),
-        ]),
     );
   }
+
+
+//  @override
+//  Widget build(BuildContext context) {
+//    return NestedScrollView(
+//      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
+//        return [
+//          SliverAppBar(
+//            floating: true,
+//            pinned: true,
+//            title: Text('subscription',
+//                style: TextStyle(color: Colors.white, fontFamily: 'Lobster')),
+//            centerTitle: true,
+//            backgroundColor: const Color(0xffff26c9),
+//            bottom: PreferredSize(
+//                child: Container(
+//                  height: 150.0,
+//                  child: new MyTabBar(
+//                    labelColor: Colors.red,
+//                    unselectedLabelColor: Colors.grey[100],
+//                    indicator: KaiyanIndictor(w: 150.0, color: Colors.red),
+//                    tabs: <MyTab>[
+//                      new MyTab(
+//                        child: Container(
+//                          child: Center(child: Text("作品", style: TextStyle(fontSize: 40.0),)),
+//                          decoration: new BoxDecoration(image: new DecorationImage(image: new ExactAssetImage(GSYICons.HOME_TOP_BG), fit: BoxFit.cover)),
+//                        )),
+//                      new MyTab(
+//                          child: Container(
+//                            child: Center(child: Text("动态", style: TextStyle(fontSize: 40.0))),
+//                            decoration: new BoxDecoration(image: new DecorationImage(image: new ExactAssetImage(GSYICons.HOME_TOP_BG), fit: BoxFit.cover)),
+//                          )),
+//                    ],
+//                    controller: _tabController,
+//                  ),
+//                ),
+//                preferredSize: Size(double.infinity,175.0)
+//            ),
+//          )
+//        ];
+//      },
+//      body: TabBarView(
+//        controller: _tabController,
+//        children: [
+//          Works(),
+//          Dynamic(),
+//        ]),
+//    );
+//  }
 
 }
